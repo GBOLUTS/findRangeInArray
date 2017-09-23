f1f2f656393092e7a2d0623e24ecae96e87d3417
GBOLUTS <bol_gena@mail.ru>
2017-09-23 23:35:08 +0700

    Работает но без ввода со стороны пользователя + хочу настроить правило для определения интервала

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,27 +1,47 @@
 package com.company;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+import jdk.nashorn.internal.ir.WhileNode;
+
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Integer> arr = new ArrayList<Integer> (Arrays.asList(5,6,7,8));
+        int positionToCheck=0;
+        ArrayList<Integer> arr = new ArrayList<Integer> (Arrays.asList(4,6,7,8,10,11,12,13,14,100,101));
         Collections.sort (arr);
 
-        ArrayList<String> sarr = new ArrayList<String>();
+        ArrayList<String> sArr = new ArrayList<String>();//Создаём String копию последовательности для изменения
         for (int i =0; i < arr.size(); i++){
-            sarr.add(String.valueOf(arr.get(i)));
+            sArr.add(String.valueOf(arr.get(i)));
         }
 
-        findRanges(sarr,0);
+        while(positionToCheck<sArr.size())
+        {positionToCheck=findRanges(sArr,positionToCheck);}
 
-        for (int i =0; i < sarr.size(); i++)
+
+        printBeautifulArray(sArr);
+    }
+//Методы
+    private static void printBeautifulArray(ArrayList<String> arr) {
+        for (int i = 0; i < arr.size(); i++)
         {
-            System.out.println(sarr.get(i));
+            if (i > 0){
+                System.out.print(",");
+                }else{
+                    System.out.print("(");
+                     }
+
+            System.out.print(arr.get(i));
+
+            if (i == (arr.size() - 1)){
+                System.out.println(")");
+            }
         }
     }
 
-    public static int findRanges(ArrayList<String> list, int a){
+    private static int findRanges(ArrayList<String> list, int a){
         int range = 1;
         int position = a;
         while ((position<list.size()-1) && (Integer.parseInt(list.get(position))+1==Integer.parseInt(list.get(position+1)))){
@@ -30,7 +50,7 @@ public class Main {
         }
         if (range < 3) {return a + 1;}
         else{
-            String strRange = new String(list.get(a) + "-"+ list.get(a + range));
+            String strRange = new String(list.get(a) + "-"+ list.get(a + range-1));
             list.add(a, strRange);/*Вначале вставляем Range, потом удаляем элементы после него, которые попали в него*/
             for (int i = a+1; i < a+range+1;i++){
                 list.remove(a+1);
